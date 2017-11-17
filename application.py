e6bd5495bbfa5dd76243930a251f6971dde1c454
Swapneel Chalageri <swapneel@Apples-MBP.home>
2017-11-16 23:43:19 -0500

    visualization + simple query

--- application.py
+++ application.py
@@ -6,11 +6,15 @@ Author: Scott Rodkey - rodkeyscott@gmail.com
 
 Step-by-step tutorial: https://medium.com/@rodkey/deploying-a-flask-application-on-aws-a72daba6bb80
 '''
+import sqlite3
 
-from flask import Flask, render_template, request
+
+from flask import Flask, render_template, request, g
 from application import db
 from application.models import Data
-from application.forms import EnterDBInfo, RetrieveDBInfo
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from collections import Counter
 
 # Elastic Beanstalk initalization
 application = Flask(__name__)
@@ -18,34 +22,65 @@ application.debug=True
 # change this to your own value
 application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
 
+class Recipes(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20))
+    cuisine = db.Column(db.String(20))
+    ingredient0 = db.Column(db.String(20))
+    ing0quantity = db.Column(db.String(20))
+    ingredient1 = db.Column(db.String(20))
+    ing1quantity = db.Column(db.String(20))
+    ingredient2 = db.Column(db.String(20))
+    ing2quantity = db.Column(db.String(20))
+    ingredient3 = db.Column(db.String(20))
+    ing3quantity = db.Column(db.String(20))
+    ingredient4 = db.Column(db.String(20))
+    ing4quantity = db.Column(db.String(20))
+    ingredient5 = db.Column(db.String(20))
+    ing5quantity = db.Column(db.String(20))
+    ingredient6 = db.Column(db.String(20))
+    ing6quantity = db.Column(db.String(20))
+    ingredient7 = db.Column(db.String(20))
+    ing7quantity = db.Column(db.String(20))
+
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/index', methods=['GET', 'POST'])
 def index():
-    form1 = EnterDBInfo(request.form)
-    form2 = RetrieveDBInfo(request.form)
+    result = Recipes.query.all()
 
-    if request.method == 'POST' and form1.validate():
-        data_entered = Data(notes=form1.dbNotes.data)
-        try:
-            db.session.add(data_entered)
-            db.session.commit()
-            db.session.close()
-        except:
-            db.session.rollback()
-        return render_template('thanks.html', notes=form1.dbNotes.data)
+    # below query is simple filter by, ordered by ID
+    #result = Recipes.query.filter_by(cuisine = "mexican")
+    return render_template('index.html', result=result)
 
-    if request.method == 'POST' and form2.validate():
-        try:
-            num_return = int(form2.numRetrieve.data)
-            query_db = Data.query.order_by(Data.id.desc()).limit(num_return)
-            for q in query_db:
-                print(q.notes)
-            db.session.close()
-        except:
-            db.session.rollback()
-        return render_template('results.html', results=query_db, num_return=num_return)
+@application.route("/viewdb")
+def viewdb():
+    rows = execute_query("""SELECT * FROM reciesp""")
+    return '<br>'.join(str(row) for row in rows)
 
-    return render_template('index.html', form1=form1, form2=form2)
+
+@application.route('/countme/<input_str>')
+def count_me(input_str):
+    input_counter = Counter(input_str)
+    response = []
+    for letter, count in input_counter.most_common():
+        response.append('"{}":{}'.format(letter, count))
+    return '<br>'.join(response)
+
+application.config['SQLALCHEMY_DATABASE_URI'] =  'mysql+pymysql://krajput96:cs336proj@whatshouldieat.c8rryuxgrrfa.us-east-1.rds.amazonaws.com:3306/whatshouldieat/recipes'
+db = SQLAlchemy(application)
+
+
+
+
+# DATABASE = 'mysql+pymysql://krajput96:cs336proj@whatshouldieat.c8rryuxgrrfa.us-east-1.rds.amazonaws.com:3306/whatshouldieat'
+#
+# engine = create_engine(DATABASE)
+#
+# print(engine.table_names())
+
+
+
 
 if __name__ == '__main__':
     application.run()
