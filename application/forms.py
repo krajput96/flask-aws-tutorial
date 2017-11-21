3b7347ce43edeb1509363cf711ec026e777b2dc5
rajputkaran270@gmail.com <rajputkaran270@gmail.com>
2017-11-20 19:56:29 -0500

    index info and getInfo

--- application/forms.py
+++ application/forms.py
@@ -1,26 +1,17 @@
 from flask_wtf import Form
-from wtforms import TextField, validators, SelectField, SubmitField
-from wtforms_sqlalchemy.fields import QuerySelectField
+from wtforms import TextField, validators, SelectField, SubmitField, StringField
+#from wtforms_sqlalchemy.fields import QuerySelectField
 
 
-CUISINE_CHOICES = [(' ',' '), ('Mexican', 'Mexican') , ('Italian', 'Italian'), ('Indian', 'Indian')]
-
-CALORIES_CHOICES = [(' ',' '), ('High Calories', 'High Calories'), ('Low Calories', 'Low Calories')]
-
-PROTEIN_CHOICES = [(' ',' '), ('High Protein', 'High Protein'), ('Low Protein', 'Low Protein')]
-
+CUISINE_CHOICES = [(' ',' '), ('Mexican', 'Mexican') , ('Italian', 'Italian'), ('Indian', 'Indian'), ('French', 'French'), ('Thai', 'Thai'), ('Russian', 'Russian'), ('Spanish', 'Spanish'), ('Japanese', 'Japanese'), ('British', 'British'), ('Korean', 'Korean'), ]
 CALORIES_PROTEIN_CHOICES = [(' ',' '), ('High Protein', 'High Protein'), ('Low Protein', 'Low Protein'), ('High Calories', 'High Calories'), ('Low Calories', 'Low Calories')]
 
-class cuisineForm(Form):
-    cuisine = SelectField(label='Cuisine', choices=CUISINE_CHOICES)
-    submit = SubmitField("Submit")
-
-class caloriesForm(Form):
-    calories = SelectField(label='Calories', choices=CALORIES_CHOICES)
-    submit = SubmitField("Submit")
-
 class complexForm(Form):
-    cuisine = SelectField(label='Cuisine', choices=CUISINE_CHOICES)
-    calories_protein = SelectField(label='Calories', choices=CALORIES_PROTEIN_CHOICES)
+    cuisine = SelectField(label='Cuisine: ', choices=CUISINE_CHOICES)
+    calories_protein = SelectField(label='Choose: ', choices=CALORIES_PROTEIN_CHOICES)
 
     submit = SubmitField("Submit")
+
+class moreInformation(Form):
+    moreInfo = StringField('ID', validators = [validators.DataRequired()])
+    submit = SubmitField('Submit')
