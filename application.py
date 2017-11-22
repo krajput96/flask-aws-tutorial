1e3dc3b3315d3d612da91a6108ac20785838d621
Swapneel Chalageri <swapneel@nbp-225-251.nbp.ruw.rutgers.edu>
2017-11-22 02:17:30 -0500

    no

--- application.py
+++ application.py
@@ -12,12 +12,12 @@ from collections import Counter
 from application.forms import complexForm, moreInformation, patternVerf, addFridge
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from flask.ext.wtf import Form
-# from flask.ext.bootstrap import Bootstrap
+from flask.ext.bootstrap import Bootstrap
 #from application.models import Fridge, Store, Recipes
 
 # Elastic Beanstalk initalization
 application = Flask(__name__)
-# Bootstrap(application)
+Bootstrap(application)
 application.debug=True
 # change this to your own value
 application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
